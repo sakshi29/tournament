16b0e0307d38c9633d35c1f5d0e26ff08d157b8c
sakshi tripathi <sakshi.trp@gmail.com>
2016-10-29 11:38:38 -0700

    Add files via upload

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,46 +6,36 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
---Drop 
-DROP DATABASE IF EXISTS tournament;
---create database.
-CREATE DATABASE tournament;
 
---connect to database tournament
-\c tournament
+--create database.
+ CREATE DATABASE tournament;
 
 --table for players record.
-CREATE TABLE players_detail( player_id SERIAL PRIMARY KEY,
-                             name TEXT );
+CREATE TABLE players_detail( player_id serial primary key,
+                             name text );
 
 --table to keep record of matches.
-CREATE TABLE match( match_id SERIAL PRIMARY KEY,
-                    winner INTEGER REFERENCES players_detail(player_id),
-                    loser INTEGER REFERENCES players_detail(player_id)
+CREATE TABLE match( match_id serial primary key,
+                    winner integer references players_detail(player_id),
+                    loser integer references players_detail(player_id)
 );
 
--- Create View 
---It keep record of no of win of each player
+
 CREATE VIEW wins AS 
-SELECT players_detail.id , name , count(winner) AS wins
+SELECT player_id , name , count(winner) as wins
 FROM players_detail LEFT JOIN match
-ON players_detail.id = winner
-GROUP BY players_detail.id;
+ON player_id = winner
+GROUP BY player_id;
 
---Create View
---It keep the record of number of losses
 CREATE VIEW loss AS 
-SELECT players_detail.id , name , count(loser) AS loss
+SELECT player_id , name , count(loser) as loss
 FROM players_detail LEFT JOIN match
-ON players_detail.id = loser
-GROUP BY players_detail.id;
+ON player_id = loser
+GROUP BY player_id;
 
---Create View
---It keep recored of player_id , name , wins(total no of game won by player_id) , matches(total number of game played by player_id)
--- in descending order of win
 
 CREATE VIEW playerStandings AS 
-SELECT wins.id,wins.name,wins.wins,(wins.wins + loss.loss) AS matches
+SELECT wins.player_id,wins.name,wins.wins,(wins.wins + loss.loss) AS matches
 FROM wins , loss 
-WHERE wins.id=loss.id 
+WHERE wins.player_id=loss.player_id 
 order by wins DESC;
